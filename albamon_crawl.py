c488d643eb4b592c0c07ca487ff8fd8a7297a4e5
mugju <ddaa1155k@gmail.com>
2021-05-29 03:09:22 +0900

    수정

--- albamon_crawl.py
+++ albamon_crawl.py
@@ -10,18 +10,22 @@ session = requests.Session()
 
 title = [] #알바title = [] #알바 제목
 
-result = pd.DataFrame(columns=['근무지역', '근무회사', '근무시간','급여','올린시간','알바설명'])  # 결과저장용 DataFrame
+result = pd.DataFrame(columns=['지역', '근무회사', '근무시간','급여','올린시간','알바설명'])  # 결과저장용 DataFrame
+new_df = pd.DataFrame(columns=['지역', '근무회사', '근무시간','급여','올린시간','알바설명'])  # append용 DataFrame
 
-area_list = []
-cName_list = []
-cTit_list = []
-pay_list = []
-time_list = []
-recently_list = []
 
 pagenum = 1 #첫 페이지
-while(pagenum < 2):  #n개 페이지에 대하여 크롤링
-    url = 'https://www.albamon.com/list/gi/mon_area_list.asp?page=1&ps=20&ob=6&lvtype=1&rArea=,B210&rWDate=1&Empmnt_Type=' #이건 알바천국
+while(pagenum < 3):  #n개 페이지에 대하여 크롤링
+    print(pagenum)
+    
+    # 리스트 초기화
+    area_list = []
+    cName_list = []
+    cTit_list = []
+    pay_list = []
+    time_list = []
+    recently_list = []
+    url = 'https://www.albamon.com/list/gi/mon_area_list.asp?page=argu&ps=20&ob=6&lvtype=1&rArea=,B210&rWDate=1&Empmnt_Type=' #이건 알바천국
     url = url.replace('argu', str(pagenum))  # 페이지 1에대한 경우 이후 이는 변수로 처리할 것.
 
     pagenum = pagenum + 1 #다음페이지 준비
@@ -79,24 +83,19 @@ while(pagenum < 2):  #n개 페이지에 대하여 크롤링
     for ww in recently:
         recently_list.append(ww.get_text().strip())
         #print(ww.get_text().strip())
+    
+    
+    new_df['지역'] = area_list
+    new_df['근무회사'] = cName_list
+    new_df['근무시간'] = time_list
+    new_df['급여'] = pay_list
+    new_df['올린시간'] = recently_list
+    new_df['알바설명'] = cTit_list    
+    print(new_df)
+
+    result = pd.concat([result,new_df])
+    #result.reset_index([range(result.len())])
 
 
-
-# area_list = []
-# cName_list = []
-# cTit_list = []
-# pay_list = []
-# time_list = []
-# recently_list = []
-
-print(area_list)
-print(time_list)
-# result = pd.DataFrame(columns=['근무지역', '근무회사', '근무시간','급여','올린시간','알바설명'])  # 결과저장용 DataFrame
-result['근무지역'] = area_list
-result['근무회사'] = cName_list
-result['근무시간'] = time_list
-result['급여시간'] = pay_list
-result['올린시간'] = recently_list
-result['알바설명'] = cTit_list
 
 print(result)
